374110770638992da81bfae0d55cb6bdd806b65c
Christopher Garoutte <garouttc@oregonstate.edu>
2020-04-21 21:26:05 -0700

    clean up directory structure

--- __init__.py
+++
@@ -1,8 +0,0 @@
-"""the public module that includes views"""
-from flask import Flask
-
-app = Flask(__name__, template_folder='template')
-
-from webapp.frontend.views import mod
-
-app.register_blueprint(frontend.views.mod)
--- app/app.py
+++ app/app.py
@@ -1,9 +1,35 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, make_response, Blueprint
+from api.ProjectsAPI import projects_api
+from api.ObservationsAPI import observations_api
+from api.functions.dbconnection import *
+from json2html import *
+import json
+from frontend.views import mod
 
-@app.route('/')
-def hello_world():
-    return 'Flask app is working'
+## ----------------------------------------------------------------------------------------##
+## Code Reference Credit
+## ----------------------------------------------------------------------------------------##
+##
+## Blueprint Configuratiton for multiple API files:
+## https://stackoverflow.com/questions/15231359/split-python-flask-app-into-multiple-files
+##
+## Code for adding result headers Sample taken from:
+## https://stackoverflow.com/questions/43796423/python-converting-mysql-query-result-to-json
+##
+## ----------------------------------------------------------------------------------------##
+
+def create_app():
+    app = Flask(__name__, template_folder='template')
+    app.register_blueprint(projects_api)
+    app.register_blueprint(observations_api)
+    app.register_blueprint(mod)
+    return app
+
+app = create_app()
+
+@app.route("/")
+def root():
+    return "Home Route"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='127.0.0.1', port=8080, debug=True)
