53b84d057871a20ff64ba3ef8c8ad64acf113e5c
captnAHAD <ahadley01@gmail.com>
2020-04-27 22:28:19 -0600

    attempted clean-up of extraneous files

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,59 +20,13 @@ function Home(){
 
     return (
         <div>
-            <>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar>
-            <br />
-            <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
-                </Form>
-            </Navbar>
-
-            <br />
-            <Navbar bg="light" variant="light">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-primary">Search</Button>
-                </Form>
-            </Navbar>
-            </>
-
-
-            <div>
-                {data.map(data => ( 
-                    <div key={data.title}>
-                        <h3>{data.title}</h3>
-                        <div>{data.date}</div>
-                        <div>{data.notes}</div>
-                    </div>
-                ))}
-            </div>
-            
+            {data.map(data => ( 
+                <div key={data.title}>
+                    <h3>{data.title}</h3>
+                    <div>{data.date}</div>
+                    <div>{data.notes}</div>
+                </div>
+            ))}
         </div>
     )
 }
