0dc89930c46eeecf92f555be4d9959ec89d68087
bshimmm <shimb@oregonstate.edu>
2020-05-15 13:37:14 -0400

    formatted homepage

---
+++ client/src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import earth from './earth.png';
+
+const Logo = () => {
+    return (
+        <div className='Home'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: '5px'}} alt='logo' src={earth}/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import history from '../components/history';
+import CarouselComponent from "../components/Carousel/Carousel";
+import './Home.css';
 
-function Home(){
-    return (
-        <div>
-            Home - Give an intro to the website (ie logo and description), along with a "view projects" button
-        </div>
-    )
-}
+    const Home = () => {
+        return (
+                <div>
+                    <h1 class='tc'>{'Citizen Science for Kids!'}</h1>
+                    <div className='tc'>
+                            <button onClick={() => history.push('/projects')} className='w-30 grow f4 link ph3 pv2 dib white bg-light-green center'>View Projects</button>
+                    </div>
+                    <br/>
+                    <CarouselComponent />
+                </div>
+        );
+    }
 
 export default Home;
