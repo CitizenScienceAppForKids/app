a71c1d9520cbe4161a79e1ae9d5c138001b67989
Christopher Garoutte <garouttc@oregonstate.edu>
2020-05-31 14:39:52 -0700

    remove navigation route from service worker

--- client/src/sw-template.js
+++ client/src/sw-template.js
@@ -10,24 +10,19 @@ if ('function' === typeof importScripts) {
 
     /* injection point for manifest files.  */
     workbox.precaching.precacheAndRoute([]);
-//
-//    /* custom cache rules*/
-//    workbox.routing.registerNavigationRoute('/index.html', {
-//      blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/],
-//    });
-//
-//    workbox.routing.registerRoute(
-//      /\.(?:png|gif|jpg|jpeg)$/,
-//      workbox.strategies.cacheFirst({
-//        cacheName: 'images',
-//        plugins: [
-//          new workbox.expiration.Plugin({
-//            maxEntries: 60,
-//            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//          }),
-//        ],
-//      })
-//    );
+
+    workbox.routing.registerRoute(
+      /\.(?:png|gif|jpg|jpeg)$/,
+      workbox.strategies.cacheFirst({
+        cacheName: 'images',
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
+      })
+    );
 
 } else {
     console.log('Workbox could not be loaded. No Offline support');
